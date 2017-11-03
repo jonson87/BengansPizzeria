5e9d5e29c4c137089a68f5bae442563ca5e50665
Erik Jonson <erik.jonson87@gmail.com>
2017-11-03 14:35:00 +0100

    GetWinnerOfTheYear startup

--- BengansBowlingHallDbLib/BengansSystem.cs
+++ BengansBowlingHallDbLib/BengansSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using AccountabilityLib;
+using System.Linq;
 using BengansBowlingHallDbLib.Interfaces;
 
 namespace BengansBowlingHallDbLib
@@ -109,6 +110,28 @@ namespace BengansBowlingHallDbLib
             }
 
             return match.Rounds[0].PlayerTwoSerie.Player;
+        }
+
+        public Party GetWinnerOfTheYear(int year)
+        {
+            var playerList = new List<PlayerStatistics>();
+            var competitionsThisYear = _repository.GetAllCompetitions().Where(x=>x.Period.Starttime.Year == year && x.Period.Endtime.Year == year);
+            foreach (var competition in competitionsThisYear)
+            {
+                foreach (var rounds in competition.Matches.Select(x=>x.Rounds))
+                {
+                    var playerStatistics = new PlayerStatistics();
+                    if (rounds.Select(x=>x.PlayerOneSerie.Player.Id) == rounds.Select(x=>x.Match.Winner.Id))
+                    {
+                        playerStatistics.MatchesWon += 1;
+                        playerStatistics.Matchesplayed += 1;
+                    }
+                    else if(rounds.Select(x=>x.PlayerTwoSerie.Player.Id) == rounds.Select(x=>x.Match.Winner.Id))
+                    {
+                        
+                    }
+                }
+            }
         }
     }
 }
---
+++ BengansBowlingHallDbLib/PlayerStatistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AccountabilityLib;
+
+namespace BengansBowlingHallDbLib
+{
+    public class PlayerStatistics
+    {
+        public Party Player { get; set; }
+        public int MatchesWon { get; set; }
+        public int Matchesplayed { get; set; }
+
+        public decimal WinPercent => MatchesWon / Matchesplayed;
+    }
+}
