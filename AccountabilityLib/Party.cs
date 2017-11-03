cd6cc3244cc5277f62d2aa4b1d88c5e6aa75812f
DannyBoy <d_iliovits@windowslive.com>
2017-11-03 11:34:55 +0100

    Right property name now!

--- AccountabilityLib/Party.cs
+++ AccountabilityLib/Party.cs
@@ -2,7 +2,7 @@
 {
     public class Party
     {
-        public int PartyId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string LegalId { get; set; }
     }
