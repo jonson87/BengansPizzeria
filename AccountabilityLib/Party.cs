01f46d78d2a7d5d57b71c23d612ca9e275d099dc
DannyBoy <d_iliovits@windowslive.com>
2017-11-03 10:17:32 +0100

    Removed unnecessary properties from Party.

--- AccountabilityLib/Party.cs
+++ AccountabilityLib/Party.cs
@@ -6,8 +6,6 @@ namespace AccountabilityLib
     {
         public int PartyId { get; set; }
         public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
         public string LegalId { get; set; }
     }
 }
