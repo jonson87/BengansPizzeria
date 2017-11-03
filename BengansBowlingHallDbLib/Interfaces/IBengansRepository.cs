4441e6c88dafc0deb1f5ee6f5fb863b706f52310
DannyBoy <d_iliovits@windowslive.com>
2017-11-03 10:12:26 +0100

    Created methods to get data from the repositories

--- BengansBowlingHallDbLib/Interfaces/IBengansRepository.cs
+++ BengansBowlingHallDbLib/Interfaces/IBengansRepository.cs
@@ -6,10 +6,18 @@ namespace BengansBowlingHallDbLib.Interfaces
     public interface IBengansRepository
     {
         void CreateMember(string legalId, string name);
+        List<Party> GetAllParties();
+
         void CreateMatch(Party player1, Party player2);
+        List<Match> GetAllMatches();
+
         void CreateSerie(Serie serie);
+        List<Serie> GetAllSeries();
+
         void CreateRound(Round round);
+        List<Round> GetAllRounds();
+
         void CreateCompetition(string name, TimePeriod period, List<Match> matches);
-        List<Party> GetPlayers();
+        List<Competition> GetAllCompetitions();
     }
 }
--- BengansBowlingHallDbLib/Repositories/MemoryRepository.cs
+++ BengansBowlingHallDbLib/Repositories/MemoryRepository.cs
@@ -84,5 +84,30 @@ namespace BengansBowlingHallDbLib.Repositories
         {
             throw new NotImplementedException();
         }
+
+        public List<Party> GetAllParties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Match> GetAllMatches()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> GetAllSeries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Round> GetAllRounds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Competition> GetAllCompetitions()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
